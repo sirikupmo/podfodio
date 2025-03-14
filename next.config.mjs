d2ac28fd912b50aa31848ebb7dadc5c9424f4356
sirikupmo <sirikupmo@loxley.co.th>
2025-03-14 22:52:12 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // แปลงเป็น Static HTML สำหรับ GitHub Pages
+  images: { unoptimized: true }, // ปิด Image Optimization
+  assetPrefix: isProd ? '/podfodio/' : '',
+  basePath: isProd ? '/podfodio' : '',
+  env: {
+    portDev: '',
+    liffId: '',
+  },
+};
 
 export default nextConfig;
